758535c9cd9c52b668c9833f8738b1c7b05f1de2
Venkatesan R S <96675735+Tyzen-10@users.noreply.github.com>
2024-09-25 14:44:17 +0530

    re-render when window resize

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import {
   SphereGeometry,
   WebGLRenderer,
 } from "three";
-
+import "./style.css"
 const scene = new Scene();
 
 /* Create a Sphere */
@@ -25,8 +25,15 @@ const material = new MeshStandardMaterial({
 const mesh = new Mesh(geometry, material);
 scene.add(mesh);
 
+//Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+}
+
 //Camera
-const camera = new PerspectiveCamera(45, 800 / 600);
+// const camera = new PerspectiveCamera(45, 800 / 600);
+const camera = new PerspectiveCamera(45, sizes.width / sizes.height);
 camera.position.z = 20; // if this is not given, then sphere and camera will be in same position. -> so won't be able to see anything on scene.
 scene.add(camera);
 //fov, and aspect ratios.
@@ -43,5 +50,20 @@ scene.add(light);
 const canvas = document.querySelector(".webgl");
 const renderer = new WebGLRenderer({ canvas });
 //how big canvas is and where to render it - code below
-renderer.setSize(800, 600);
+// renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera);
+
+//Resize
+window.addEventListener('resize', () => {
+  //update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  //update camera
+  camera.aspect = sizes.width/ sizes.height;
+  camera.updateProjectionMatrix() // what does this do? 
+  renderer.setSize(sizes.width, sizes.height);
+
+  renderer.render(scene,camera); // this will re-render on resize.
+})
